77c8d8445d1d54370ac89466837c145443222409
moeyuz <88334660+moeyuz@users.noreply.github.com>
2024-09-22 09:07:29 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -47,7 +47,7 @@ function Footer() {
                 <p className='text-sm text-neutral-500 font-normal link-line'>
                     <span>
                         © 2024 Powered by <a className='hover:underline' href="https://github.com/openRin/Rin" target="_blank">Rin</a>
-                    </span>
+                    </span> <span id="span"></span>
                     {config.get<boolean>('rss') && <>
                         <Spliter />
                         <Popup trigger={
@@ -85,6 +85,24 @@ function Footer() {
                 </div>
             </div>
         </footer>
+<script type="text/javascript">
+    function runtime(){
+        X = new Date("22/09/2024 00:00:00");
+        Y = new Date();
+        T = (Y.getTime()-X.getTime());
+        M = 24*60*60*1000;
+        a = T/M;
+        A = Math.floor(a);
+        b = (a-A)*24;
+        B = Math.floor(b);
+        c = (b-B)*60;
+        C = Math.floor((b-B)*60);
+        D = Math.floor((c-C)*60);
+        //信息写入到DIV中
+        span.innerHTML = "本网站己运行: "+A+"天"+B+"小时"+C+"分"+D+"秒"
+    }
+    setInterval(runtime, 1000);
+</script>
     );
 }
 
